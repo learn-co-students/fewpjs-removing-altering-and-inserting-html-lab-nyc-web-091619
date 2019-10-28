16eb18577b46599a7b97871f0769cbfbfe313561
maryfarner18 <maryfarner@gmail.com>
2019-10-28 17:21:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Mary is the champion"
+
+document.body.appendChild(newHeader);
