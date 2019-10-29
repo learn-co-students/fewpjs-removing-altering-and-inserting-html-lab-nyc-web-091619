5fbac6f12ae01c19b28bbdfc8c8d325d30aa8a18
Michelle Lee <mlee092@Michelles-MacBook.local>
2019-10-28 20:57:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+// ✓ no longer has DOM node 'main#main'
+document.querySelector("main#main").remove();
+
+
+//✓ has a 'newHeader' variable that points to node 'h1#victory'
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+//✓ has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+newHeader.innerHTML = "Michelle is the champion!"
+newHeader.className = "Victory";
