f338a9176e107caffa0102b4ff150de2f6442066
Altaf Quadri <altafmquadri@gmail.com>
2019-10-29 23:57:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.getElementById("main").remove()
+
+let newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+newHeader.innerHTML = "Altaf is the champion"
+
+
+document.body.appendChild(newHeader)
